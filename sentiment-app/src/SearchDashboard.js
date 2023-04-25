abe8ac3c2176d3bc7e7e50b2e9db1a0a916a5d34
Sam <Sam Singh Anantha>
2023-04-24 22:29:31 -0400

    added line graph

--- sentiment-app/src/SearchDashboard.js
+++ sentiment-app/src/SearchDashboard.js
@@ -1,50 +1,70 @@
-import React, { Component } from 'react';
-import SearchCard  from './SearchCard';
-import axios from 'axios';
-import ApprovalPieGraph from './ApprovalPieGraph';
+import React, { Component } from "react";
+import SearchCard from "./SearchCard";
+import axios from "axios";
+import ApprovalPieGraph from "./ApprovalPieGraph";
 import { Route, useNavigate, useLocation, Link } from "react-router-dom";
+import LineGraph from "./LineGraph";
 
 const BASE_URL = process.env.REACT_APP_API_URL;
 
-class SearchDashboard extends Component{
-    constructor(props){
-        super(props);
-    }
+class SearchDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: props.location.state?.search.keyword,
+      subreddit: props.location.state?.search.subreddit,
+      results: [],
+    };
+  }
 
-    render(){
-        return(
-            <div style={DashboardContainerStyle}>
-                <SearchCard search={this.props.location.state?.search}/>
-                <div style={CommentContainerStyle}>
-                    <p>Positive Comment: </p>
-                    <p>{this.props.location.state?.search.comments.positive}</p>
-                </div>
-                <div style={CommentContainerStyle}>
-                    <p>Negative Comment: </p>
-                    <p>{this.props.location.state?.search.comments.negative}</p>
-                </div>
-            </div>
-        )
+  async componentDidMount() {
+    // GET results from the endpoint
+    try {
+      let response = await axios.get(BASE_URL + "/results", {
+        params: {
+          keyword: this.state.keyword,
+          subreddit: this.state.subreddit,
+        },
+      });
+      let results = JSON.parse(response.data.body);
+      console.log(results);
+      this.setState({ results: results });
+    } catch (err) {
+      console.log(err);
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row m-3 mx-4 mt-4 px-5 py-4">
+          <div className="col-lg-3">
+            <SearchCard search={this.props.location.state?.search} />
+          </div>
+
+          <div className="col">
+            <p>Positive Comments: </p>
+            {this.state.results.map((data, index) => (
+              <p>{data.comments.positive.substring(0,100)}</p>
+            ))}
+          </div>
+          <div className="col">
+            <p>Negative Comment: </p>
+            <p>{this.props.location.state?.search.comments.negative}</p>
+          </div>
+        </div>
+        <div  className="row m-3 mx-4 mt-4 px-5 py-4">
+            <LineGraph data={this.state.results}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 function SearchDashboardWithNavigation(props) {
-    let navigate = useNavigate();
-    let location = useLocation();
-    return <SearchDashboard {...props} navigate={navigate} location={location}/>
+  let navigate = useNavigate();
+  let location = useLocation();
+  return <SearchDashboard {...props} navigate={navigate} location={location} />;
 }
 
 export default SearchDashboardWithNavigation;
-
-const DashboardContainerStyle = {
-    display: 'flex'
-}
-
-const HeaderContainerStyle = {
-
-}
-
-const CommentContainerStyle = {
-    border: '2px solid black',
-    borderRadius: '10px',
-}
